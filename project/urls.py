e95d4c98f7fe855ca6589b469061361af510c052
jfilippo06 <jfilippo04@gmail.com>
2023-09-12 21:12:43 +0200

    init sistema

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('login.urls')),
     path('admin/', admin.site.urls),
+    path('', include('login.urls')),
+    path('home/', include('sistema.urls')),
 ]
