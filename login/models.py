b714fb3ab609e28f550c6e27250aaba2302add60
jfilippo06 <jfilippo04@gmail.com>
2023-11-20 18:34:01 +0100

    login

--- login/models.py
+++ login/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class UserProfile(AbstractUser):
+    ADMIN = 'admin'
+    EMPLOYEE = 'employee'
+    CLIENT = 'client'
+    USER_TYPE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (EMPLOYEE, 'Employee'),
+        (CLIENT, 'Client'),
+    ]
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE_CHOICES,
+        default=CLIENT,
+    )
+
+    REQUIRED_FIELDS = ['user_type']
