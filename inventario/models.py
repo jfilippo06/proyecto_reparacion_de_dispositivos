4936639e8996e06796735d07f9ab05cde309284a
jfilippo06 <jfilippo04@gmail.com>
2023-11-26 21:14:18 +0100

    inventario models

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Inventario(models.Model):
+    CATEGORIAS = [
+        ('COM', 'Computadora'),
+        ('TEL', 'Telefono'),
+        ('ASE', 'Asesorios'),
+        ('RPC', 'Repuestos de Computadoras'),
+        ('RPT', 'Repuestos de Telefonos'),
+    ]
+
+    codigo = models.IntegerField(unique=True)
+    articulo = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    no_serie = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True)
+    cantidad = models.PositiveIntegerField()
+    costo = models.IntegerField()
+    categoria = models.CharField(max_length=3, choices=CATEGORIAS)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
