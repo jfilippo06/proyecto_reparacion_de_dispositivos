45137b62edb47a33a36a4c57e1278c01fd511f60
jfilippo06 <jfilippo04@gmail.com>
2023-12-05 16:51:43 +0100

    template config employee

--- inventario/views.py
+++ inventario/views.py
@@ -5,28 +5,28 @@ from django.shortcuts import render
 @admin_required
 def computadora(request):
     if request.method == 'GET':
-        return render(request, 'inventario/computadora.html', {'user_name': request.user.username})
+        return render(request, 'inventario/computadora.html', {'user_name': request.user.username, 'user_type':request.user.user_type})
 
 
 @admin_required
 def telefono(request):
     if request.method == 'GET':
-        return render(request, 'inventario/telefono.html', {'user_name': request.user.username})
+        return render(request, 'inventario/telefono.html', {'user_name': request.user.username, 'user_type':request.user.user_type})
 
 
 @admin_required
 def repuesto_computadora(request):
     if request.method == 'GET':
-        return render(request, 'inventario/repuesto_computadora.html', {'user_name': request.user.username})
+        return render(request, 'inventario/repuesto_computadora.html', {'user_name': request.user.username, 'user_type':request.user.user_type})
 
 
 @admin_required
 def repuesto_telefono(request):
     if request.method == 'GET':
-        return render(request, 'inventario/repuesto_telefono.html', {'user_name': request.user.username})
+        return render(request, 'inventario/repuesto_telefono.html', {'user_name': request.user.username, 'user_type':request.user.user_type})
 
 
 @admin_required
 def acessorio(request):
     if request.method == 'GET':
-        return render(request, 'inventario/accesorio.html', {'user_name': request.user.username})
+        return render(request, 'inventario/accesorio.html', {'user_name': request.user.username, 'user_type':request.user.user_type})
