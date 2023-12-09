e70657d1374634127105e50b78e7b14daa980b55
jfilippo06 <jfilippo04@gmail.com>
2023-12-09 16:49:02 +0100

    GET computadoras

--- inventario/views.py
+++ inventario/views.py
@@ -1,11 +1,17 @@
 from login.decorators import admin_required
 from django.shortcuts import render
+from inventario.models import Inventario
+from django.core.paginator import Paginator
 
 
 @admin_required
 def computadora(request):
     if request.method == 'GET':
-        return render(request, 'inventario/computadora.html', {'username': request.user.username, 'user_type':request.user.user_type})
+        inventario = Inventario.objects.filter(categoria='COM').exclude(is_active=False)
+        paginator = Paginator(inventario, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'inventario/computadora.html', {'username': request.user.username, 'user_type':request.user.user_type, 'inventario':page_obj})
 
 
 @admin_required
