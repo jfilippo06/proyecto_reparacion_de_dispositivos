89cddc6247c2c361afd421ecb8783a29ce1f769b
jfilippo06 <jfilippo04@gmail.com>
2023-12-17 21:11:10 +0100

    UPDATE repuesto

--- inventario/urls.py
+++ inventario/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("telefono/deshabilitar/<int:id>", views.deleteTelefono, name="deshabilitar_telefono"),
     path("telefono/actualizar/<int:id>", views.updateTelefono, name="actualizar_telefono"),
     path("repuesto_computadora/", views.repuesto_computadora, name="repuestos_computadora"),
+    path("repuesto_computadora/deshabilitar/<int:id>", views.deleteRepuestoComputadora, name="deshabilitar_repuestos_computadora"),
+    path("repuesto_computadora/actualizar/<int:id>", views.updateRepuestoComputadora, name="actualizar_repuestos_computadora"),
     path("repuesto_telefono/", views.repuesto_telefono, name="repuestos_telefono"),
     path("accesorio/", views.acessorio, name="accesorio"),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -167,7 +167,7 @@ def deleteTelefono(request, id):
     inventario.save()
     return redirect('telefono')
 
-
+@admin_required
 def updateTelefono(request, id):
     if request.method == "GET":
         inventario = get_object_or_404(Inventario, id=id)
@@ -231,7 +231,79 @@ def updateTelefono(request, id):
 @admin_required
 def repuesto_computadora(request):
     if request.method == 'GET':
-        return render(request, 'inventario/repuesto_computadora.html', {'username': request.user.username, 'user_type': request.user.user_type})
+        inventario = Inventario.objects.filter(
+            categoria='RPC').exclude(is_active=False)
+        paginator = Paginator(inventario, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'inventario/repuesto_computadora/repuesto_computadora.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario':page_obj})
+
+@admin_required
+def deleteRepuestoComputadora(request, id):
+    inventario = get_object_or_404(Inventario, id=id)
+    inventario.is_active = False
+    inventario.save()
+    return redirect('repuestos_computadora')
+
+@admin_required
+def updateRepuestoComputadora(request, id):
+    if request.method == "GET":
+        inventario = get_object_or_404(Inventario, id=id)
+        if inventario.is_active == False:
+            return redirect('repuestos_computadora')
+        return render(request, 'inventario/repuesto_computadora/editar_repuesto_computadora.html', {
+            'username': request.user.username,
+            'user_type': request.user.user_type,
+            'computadora_id': id,
+            'codigo': inventario.codigo,
+            'articulo': inventario.articulo,
+            'marca': inventario.marca,
+            'modelo': inventario.modelo,
+            'no_serie': inventario.no_serie,
+            'cantidad': inventario.cantidad,
+            'costo': inventario.costo,
+        })
+
+    elif request.method == "POST":
+        try:
+            # Obtén el objeto que quieres actualizar
+            objeto = Inventario.objects.get(pk=id)
+            codigo = request.POST['codigo']
+
+            if Inventario.objects.filter(codigo=codigo).exclude(pk=id).exists():
+                inventario = get_object_or_404(Inventario, id=id)
+                messages.error(request, 'Codigo ya existe')
+                return render(request, 'inventario/repuesto_computadora/editar_repuesto_computadora.html', {
+                    'username': request.user.username,
+                    'user_type': request.user.user_type,
+                    'computadora_id': id,
+                    'codigo': inventario.codigo,
+                    'articulo': inventario.articulo,
+                    'marca': inventario.marca,
+                    'modelo': inventario.modelo,
+                    'no_serie': inventario.no_serie,
+                    'cantidad': inventario.cantidad,
+                    'costo': inventario.costo,
+                })
+
+            # Actualiza los campos del objeto con los nuevos valores
+            objeto.codigo = request.POST['codigo']
+            objeto.articulo = request.POST['articulo']
+            objeto.marca = request.POST['marca']
+            objeto.modelo = request.POST['modelo']
+            objeto.no_serie = request.POST['no_serie']
+            objeto.cantidad = request.POST['cantidad']
+            objeto.costo = request.POST['costo']
+
+            # Guarda los cambios en la base de datos
+            objeto.save()
+
+            messages.error(request, 'Actualizado correctamente')
+            return redirect('repuestos_computadora')
+        except Inventario.DoesNotExist:
+            messages.error(
+                request, 'El objeto con el código especificado no existe.')
+            return redirect('repuestos_computadora')
 
 
 @admin_required
