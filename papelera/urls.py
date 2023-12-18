015b57878e5834835f7e4ed11013ac917c8d7992
jfilippo06 <jfilippo04@gmail.com>
2023-12-18 20:27:12 +0100

    computadora papelera

--- papelera/urls.py
+++ papelera/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("usuarios/", views.usuarios, name="papelera_usuarios"),
     path("usuarios/habilitar/<int:id>", views.habilitar_usuarios, name="habilitar_usuarios"),
-    path("usuarios/buscar/", views.buscar, name="buscar_usuario_papelera"),
+    path("inventario/computadora/", views.computadora, name="papelera_computadora"),
 ]
--- papelera/views.py
+++ papelera/views.py
@@ -2,6 +2,7 @@ from login.decorators import admin_required, employee_denied
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from login.models import UserProfile
+from inventario.models import Inventario
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -18,6 +19,14 @@ def usuarios(request):
         page_obj = paginator.get_page(page_number)
         return render(request, 'papelera/usuarios.html', {'username': request.user.username, 'usuarios': page_obj})
 
+    elif request.method == 'POST':
+        username = request.POST['table_search']
+        if username == '':
+            return redirect('papelera_usuarios')
+        user = UserProfile.objects.filter(username=username).exclude(
+            user_type='super_user').exclude(is_active=True)
+        return render(request, 'papelera/usuarios.html', {'username': request.user.username, 'usuarios': user})
+
 
 @admin_required
 @employee_denied
@@ -30,11 +39,42 @@ def habilitar_usuarios(request, id):
 
 @admin_required
 @employee_denied
-def buscar(request):
-    username = request.POST['table_search']
-    if username == '':
-        return redirect('papelera_usuarios')
-    user = UserProfile.objects.filter(username=username).exclude(
-        user_type='super_user').exclude(is_active=True)
-    return render(request, 'papelera/usuarios.html', {'username': request.user.username, 'usuarios': user})
+def computadora(request):
+    if request.method == 'GET':
+        inventario = Inventario.objects.filter(
+            categoria='COM').exclude(is_active=True)
+        paginator = Paginator(inventario, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'papelera/computadora.html', {'username': request.user.username, 'inventario': page_obj})
 
+    elif request.method == 'POST':
+        inventario = request.POST['table_search']
+        user_type = request.POST['user_type']
+        if inventario == '':
+            return redirect('papelera_computadora')
+        elif user_type == 'codigo':
+            computadora = Inventario.objects.filter(
+                categoria='COM', codigo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'articulo':
+            computadora = Inventario.objects.filter(
+                categoria='COM', articulo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'marca':
+            computadora = Inventario.objects.filter(
+                categoria='COM', marca__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'modelo':
+            computadora = Inventario.objects.filter(
+                categoria='COM', modelo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'no_serie':
+            computadora = Inventario.objects.filter(
+                categoria='COM', no_serie__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'cantidad':
+            computadora = Inventario.objects.filter(
+                categoria='COM', cantidad=inventario).exclude(is_active=True)
+        elif user_type == 'costo':
+            computadora = Inventario.objects.filter(
+                categoria='COM', costo=inventario).exclude(is_active=True)
+        paginator = Paginator(computadora, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'papelera/computadora.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario': page_obj})
