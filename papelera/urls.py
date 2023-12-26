ff6f930fe696ef485e839c15ef2e9216a44405a7
jfilippo06 <jfilippo04@gmail.com>
2023-12-26 16:35:40 +0100

    get, post, put telefono

--- papelera/urls.py
+++ papelera/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("usuarios/habilitar/<int:id>", views.habilitar_usuarios, name="habilitar_usuario"),
     path("inventario/computadora/", views.computadora, name="papelera_computadora"),
     path("inventario/habilitar/<int:id>", views.habilitar_computadoras, name="habilitar_computadora"),
+    path("inventario/telefono/", views.telefono, name="papelera_telefono"),
+    path("inventario/telefono/<int:id>", views.habilitar_telefonos, name="habilitar_telefono"),
 ]
--- papelera/views.py
+++ papelera/views.py
@@ -87,3 +87,54 @@ def habilitar_computadoras(request, id):
     user.is_active = True
     user.save()
     return redirect('papelera_computadora')
+
+@admin_required
+@employee_denied
+def telefono(request):
+    if request.method == 'GET':
+        inventario = Inventario.objects.filter(
+            categoria='TEL').exclude(is_active=True)
+        paginator = Paginator(inventario, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'papelera/telefono.html', {'username': request.user.username, 'inventario': page_obj})
+
+    elif request.method == 'POST':
+        inventario = request.POST['table_search']
+        user_type = request.POST['user_type']
+        if inventario == '':
+            return redirect('papelera_telefono')
+        elif user_type == 'codigo':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', codigo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'articulo':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', articulo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'marca':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', marca__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'modelo':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', modelo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'no_serie':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', no_serie__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'cantidad':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', cantidad=inventario).exclude(is_active=True)
+        elif user_type == 'costo':
+            computadora = Inventario.objects.filter(
+                categoria='TEL', costo=inventario).exclude(is_active=True)
+        paginator = Paginator(computadora, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'papelera/telefono.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario': page_obj})
+
+
+@admin_required
+@employee_denied
+def habilitar_telefonos(request, id):
+    user = get_object_or_404(Inventario, id=id)
+    user.is_active = True
+    user.save()
+    return redirect('papelera_telefono')
