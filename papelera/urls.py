888c4978b1bda331d7f9c4d86f39dc3e48ab5ed3
jfilippo06 <jfilippo04@gmail.com>
2023-12-26 16:45:18 +0100

    get, post, put rpc

--- papelera/urls.py
+++ papelera/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("inventario/habilitar/<int:id>", views.habilitar_computadoras, name="habilitar_computadora"),
     path("inventario/telefono/", views.telefono, name="papelera_telefono"),
     path("inventario/telefono/<int:id>", views.habilitar_telefonos, name="habilitar_telefono"),
+    path("inventario/repuesto_computadora/", views.repuesto_computadora, name="papelera_repuesto_computadora"),
+    path("inventario/repuesto_computadora/<int:id>", views.habilitar_repuesto_computadora, name="habilitar_repuesto_computadora"),
 ]
--- papelera/views.py
+++ papelera/views.py
@@ -138,3 +138,53 @@ def habilitar_telefonos(request, id):
     user.is_active = True
     user.save()
     return redirect('papelera_telefono')
+
+@admin_required
+@employee_denied
+def repuesto_computadora(request):
+    if request.method == 'GET':
+        inventario = Inventario.objects.filter(
+            categoria='RPC').exclude(is_active=True)
+        paginator = Paginator(inventario, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'papelera/repuesto_computadora.html', {'username': request.user.username, 'inventario': page_obj})
+
+    elif request.method == 'POST':
+        inventario = request.POST['table_search']
+        user_type = request.POST['user_type']
+        if inventario == '':
+            return redirect('papelera_repuesto_computadora')
+        elif user_type == 'codigo':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', codigo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'articulo':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', articulo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'marca':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', marca__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'modelo':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', modelo__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'no_serie':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', no_serie__iexact=inventario).exclude(is_active=True)
+        elif user_type == 'cantidad':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', cantidad=inventario).exclude(is_active=True)
+        elif user_type == 'costo':
+            computadora = Inventario.objects.filter(
+                categoria='RPC', costo=inventario).exclude(is_active=True)
+        paginator = Paginator(computadora, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'papelera/repuesto_computadora.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario': page_obj})
+
+@admin_required
+@employee_denied
+def habilitar_repuesto_computadora(request, id):
+    user = get_object_or_404(Inventario, id=id)
+    user.is_active = True
+    user.save()
+    return redirect('papelera_repuesto_computadora')
