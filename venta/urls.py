e057c5b7dc77487a7d2a00bfecd7f5e7fc60fec6
jfilippo06 <jfilippo04@gmail.com>
2024-01-07 00:48:09 +0100

    facturar y registrar

--- venta/urls.py
+++ venta/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('consultar_cliente/', views.client, name='cliente'),
+    path('registrar_cliente/', views.registrar_cliente, name='registrar_cliente'),
+    path('facturar_cliente/', views.facturar_cliente, name='facturar_cliente'),
 ]
--- venta/views.py
+++ venta/views.py
@@ -1,5 +1,6 @@
 from login.decorators import admin_required, employee_denied
 from django.shortcuts import render, redirect, get_object_or_404
+from venta.models import Client
 
 # Create your views here.
 
@@ -9,6 +10,31 @@ from django.shortcuts import render, redirect, get_object_or_404
 def client(request):
     if request.method == 'GET':
         return render(request, 'venta/consultar_cliente.html', {'username': request.user.username})
-    
+
+    elif request.method == 'POST':
+        cedula = request.POST['cedula']
+        if Client.objects.filter(cedula=cedula):
+            return redirect('facturar_cliente')
+        else:
+            return redirect('registrar_cliente')
+
+
+
+@admin_required
+@employee_denied
+def registrar_cliente(request):
+    if request.method == 'GET':
+        return render(request, 'venta/registrar_cliente.html', {'username': request.user.username})
+
+    elif request.method == 'POST':
+        pass
+
+
+@admin_required
+@employee_denied
+def facturar_cliente(request):
+    if request.method == 'GET':
+        return render(request, 'venta/facturar_cliente.html', {'username': request.user.username})
+
     elif request.method == 'POST':
         pass
