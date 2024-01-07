5d20e5d086053e36f3227d0433d7653ef223b231
jfilippo06 <jfilippo04@gmail.com>
2024-01-07 01:48:31 +0100

    registrar cliente

--- venta/views.py
+++ venta/views.py
@@ -1,5 +1,6 @@
 from login.decorators import admin_required, employee_denied
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from venta.models import Client
 
 # Create your views here.
@@ -13,6 +14,7 @@ def client(request):
 
     elif request.method == 'POST':
         cedula = request.POST['cedula']
+        request.session['cedula'] = cedula
         if Client.objects.filter(cedula=cedula):
             return redirect('facturar_cliente')
         else:
@@ -24,11 +26,31 @@ def client(request):
 @employee_denied
 def registrar_cliente(request):
     if request.method == 'GET':
-        return render(request, 'venta/registrar_cliente.html', {'username': request.user.username})
+        cedula = request.session.get('cedula')
+        return render(request, 'venta/registrar_cliente.html', {'username': request.user.username, 'cedula': cedula})
 
     elif request.method == 'POST':
-        pass
+        cedula = request.POST['cedula']
+        nombre = request.POST['nombre']
+        email = request.POST['email']
 
+        if Client.objects.filter(cedula=cedula).exists():
+            messages.error(request, 'Cédula ya exite.')
+            return redirect('registrar_cliente')
+        elif Client.objects.filter(email=email).exists():
+            messages.error(request, 'Correo electronico le pertenece a otro cliente.')
+            return redirect('registrar_cliente')
+
+        nuevo_cliente = Client(cedula=cedula, username=nombre, email=email)
+        nuevo_cliente.save()
+
+        return redirect('facturar_cliente')
+
+
+@admin_required
+@employee_denied
+def cancelar(request):
+    return redirect('cliente')
 
 @admin_required
 @employee_denied
