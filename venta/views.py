04662c774f818ebfc45e1b58ce04ce7fd5699d2b
jfilippo06 <jfilippo04@gmail.com>
2024-01-07 22:35:47 +0100

    POST

--- venta/views.py
+++ venta/views.py
@@ -58,13 +58,43 @@ def cancelar(request):
 @admin_required
 @employee_denied
 def facturar_cliente(request):
-    if request.method == 'GET':
+    if request.method == 'GET' and 'inventario_ids' in request.session:
+        inventario_ids = request.session['inventario_ids']
+        inventario = Inventario.objects.filter(id__in=inventario_ids)
+
+    elif request.method == 'GET':
         inventario = Inventario.objects.filter(
             categoria='COM').exclude(is_active=False)
-        paginator = Paginator(inventario, 5)
-        page_number = request.GET.get('page', 1)
-        page_obj = paginator.get_page(page_number)
-        return render(request, 'venta/facturar_cliente.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario': page_obj})
 
     elif request.method == 'POST':
-        pass
+        equipo = request.POST['equipo']
+        user_type = request.POST['user_type']
+        table_search = request.POST['table_search']
+
+        # Define a dictionary to map user_type to the corresponding field in the model
+        user_type_field_map = {
+            'codigo': 'codigo__iexact',
+            'articulo': 'articulo__iexact',
+            'marca': 'marca__iexact',
+            'modelo': 'modelo__iexact',
+            'no_serie': 'no_serie__iexact',
+            'cantidad': 'cantidad',
+            'costo': 'costo'
+        }
+
+        # Initialize the query parameters
+        query_params = {'categoria': equipo}
+
+        # If table_search is not empty, add the corresponding field to the query parameters
+        if table_search != '':
+            query_params[user_type_field_map[user_type]] = table_search
+
+        # Query the database
+        inventario = Inventario.objects.filter(**query_params).exclude(is_active=False)       
+        request.session['inventario_ids'] = list(
+            inventario.values_list('id', flat=True))
+
+    paginator = Paginator(inventario, 5)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'venta/facturar_cliente.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario': page_obj})
