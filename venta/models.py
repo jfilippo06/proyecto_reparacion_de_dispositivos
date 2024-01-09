4f7caba20936a0dc0d8bcdec8bdf0a1d6d74dc4b
jfilippo06 <jfilippo04@gmail.com>
2024-01-09 01:50:14 +0100

    model, migrations

--- venta/models.py
+++ venta/models.py
@@ -6,4 +6,14 @@ class Client(models.Model):
     cedula = models.IntegerField()
     username = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
+
+class N_Recibo(models.Model):
+    n_recibo = models.IntegerField()
+
+class T_Lista(models.Model):
+    articulo = models.CharField(max_length=50)
+    cantidad = models.PositiveIntegerField()
+    costo_unidad = models.IntegerField()
+    total = models.IntegerField()
+    n_recibo = models.OneToOneField(N_Recibo, on_delete=models.CASCADE)
     
