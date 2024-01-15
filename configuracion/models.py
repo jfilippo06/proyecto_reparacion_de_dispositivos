361f20c458448c6d279df2b5b27e72db842fdc7a
jfilippo06 <jfilippo04@gmail.com>
2024-01-15 16:24:31 +0100

    model impuesto

--- configuracion/models.py
+++ configuracion/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Impuesto(models.Model):
+    iva = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
