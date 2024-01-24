761a9b9ef2406b19f2c021808a2e0c15db8bec68
jfilippo06 <jfilippo04@gmail.com>
2024-01-24 18:44:55 +0100

    post reporte

--- reporte/views.py
+++ reporte/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from login.decorators import admin_required, employee_denied
 from venta.models import Direccion_de_factura
 from django.core.paginator import Paginator
@@ -8,8 +8,21 @@ from django.core.paginator import Paginator
 @admin_required
 @employee_denied
 def recibo(request):
-    direccion = Direccion_de_factura.objects.all()
-    paginator = Paginator(direccion, 5)
-    page_number = request.GET.get('page', 1)
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'reporte/recibo.html', {'username': request.user.username, 'user_type': request.user, 'reporte': page_obj})
+    if request.method == 'GET':
+        direccion = Direccion_de_factura.objects.all()
+        paginator = Paginator(direccion, 5)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'reporte/recibo.html', {'username': request.user.username, 'user_type': request.user, 'reporte': page_obj})
+
+    elif request.method == 'POST':
+        reporte = request.POST['table_search']
+        user_type = request.POST['user_type']
+        if reporte == '':
+            return redirect('recibo')
+        else:
+            computadora = Direccion_de_factura.objects.filter(**{user_type+'__iexact': reporte})
+        paginator = Paginator(computadora, 15)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'reporte/recibo.html', {'username': request.user.username, 'user_type': request.user, 'reporte': page_obj})
