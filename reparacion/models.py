8ab56de42cece82159a0367f800e2070f249e382
jfilippo06 <jfilippo04@gmail.com>
2024-01-29 15:27:51 +0100

    model init

--- reparacion/models.py
+++ reparacion/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Reparacion(models.Model):
+    CATEGORIAS = [
+        ('ER', 'En_Revision'),
+        ('EC', 'En_Curso'),
+        ('TR', 'Terminado'),
+    ]
+
+    articulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    cedula = models.IntegerField()
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    estado = models.CharField(max_length=3, choices=CATEGORIAS)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField()
