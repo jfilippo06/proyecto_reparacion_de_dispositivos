127e26992d785e55a9d9343a0d5b419e59520a58
jfilippo06 <jfilippo04@gmail.com>
2024-02-06 19:48:41 +0100

    views

--- reporte/urls.py
+++ reporte/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('recibo/', views.recibo, name='recibo'),
-    path('reparaciones/', views.reporteRepaciones, name='reporte_reparaciones')
+    path('reparaciones/', views.reporteRepaciones, name='reporte_reparaciones'),
+    path('clientes/', views.reporteCliente, name='reporte_cliente'),
+    path('inventario/', views.reporteInventario, name='reporte_inventario'),
+    path('ventas/', views.reporteVenta, name='reporte_venta'),
 ]
--- reporte/views.py
+++ reporte/views.py
@@ -6,6 +6,7 @@ from reparacion.models import Reparacion
 
 # Create your views here.
 
+
 @admin_required
 @employee_denied
 def recibo(request):
@@ -22,12 +23,16 @@ def recibo(request):
         if reporte == '':
             return redirect('recibo')
         else:
-            computadora = Direccion_de_factura.objects.filter(**{user_type+'__iexact': reporte})
+            computadora = Direccion_de_factura.objects.filter(
+                **{user_type+'__iexact': reporte})
         paginator = Paginator(computadora, 15)
         page_number = request.GET.get('page', 1)
         page_obj = paginator.get_page(page_number)
         return render(request, 'reporte/recibo.html', {'username': request.user.username, 'user_type': request.user, 'reporte': page_obj})
 
+
+@admin_required
+@employee_denied
 def reporteRepaciones(request):
     if request.method == 'GET':
         reparacion = Reparacion.objects.all()
@@ -39,3 +44,39 @@ def reporteRepaciones(request):
         pass
 
     return render(request, 'reporte/reparacion.html', {'username': request.user.username, 'reparaciones': page_obj})
+
+
+@admin_required
+@employee_denied
+def reporteCliente(request):
+    if request.method == 'GET':
+        pass
+
+    elif request.method == 'POST':
+        pass
+
+    return render(request, 'reporte/cliente.html', {'username': request.user.username})
+
+
+@admin_required
+@employee_denied
+def reporteInventario(request):
+    if request.method == 'GET':
+        pass
+
+    elif request.method == 'POST':
+        pass
+
+    return render(request, 'reporte/inventario.html', {'username': request.user.username})
+
+
+@admin_required
+@employee_denied
+def reporteVenta(request):
+    if request.method == 'GET':
+        pass
+
+    elif request.method == 'POST':
+        pass
+
+    return render(request, 'reporte/venta.html', {'username': request.user.username})
