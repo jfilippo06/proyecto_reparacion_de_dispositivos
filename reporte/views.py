e63040e640e46fe64a8b515dff9bb37e2a9d0dd6
jfilippo06 <jfilippo04@gmail.com>
2024-02-15 16:14:31 +0100

    *

--- reporte/views.py
+++ reporte/views.py
@@ -7,6 +7,13 @@ from venta.models import Totales
 from venta.views import impuesto
 from django.db.models import Q
 from datetime import datetime, time
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from io import BytesIO
 
 
 # Create your views here.
@@ -47,6 +54,7 @@ def reporteRepaciones(request):
         paginator = Paginator(reparacion, 15)
         page_number = request.GET.get('page', 1)
         page_obj = paginator.get_page(page_number)
+        return render(request, 'reporte/reparacion.html', {'username': request.user.username, 'reparaciones': page_obj})
 
     elif request.method == 'POST':
         date_begin = datetime.strptime(request.POST.get('date_begin', ''), '%Y-%m-%d') if request.POST.get(
@@ -76,10 +84,64 @@ def reporteRepaciones(request):
             paginator = Paginator(reparaciones, 15)
             page_number = request.GET.get('page', 1)
             page_obj = paginator.get_page(page_number)
+            return render(request, 'reporte/reparacion.html', {'username': request.user.username, 'reparaciones': page_obj})
         elif 'enviar' in request.POST['submit_button']:
-            pass
+            # Crea un objeto de archivo en memoria
+            buffer = BytesIO()
 
-    return render(request, 'reporte/reparacion.html', {'username': request.user.username, 'reparaciones': page_obj})
+            # Crea el archivo PDF usando ReportLab
+            doc = SimpleDocTemplate(buffer, pagesize=letter,
+                                    rightMargin=72, leftMargin=72)
+
+            # Contenedor para los elementos 'Flowable'
+            elements = []
+
+            # Usa un estilo predeterminado y agrega algunos elementos
+            styles = getSampleStyleSheet()
+
+            # Modifica el estilo del título para alinearlo al centro
+            styles["Title"].alignment = 1  # 1 = TA_CENTER
+
+            # Agrega un título
+            title = Paragraph("Título", styles["Title"])
+            elements.append(title)
+
+            # Agrega un espacio
+            elements.append(Spacer(1, 50))
+
+            # Define los datos de la tabla
+            data = [
+                [' ', ' ', ' ', ' ']
+            ]
+
+            # Crea la tabla
+            table = Table(data, colWidths=[100, 100, 100, 100])
+
+            # Define el estilo de la tabla
+            style = TableStyle([
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ])
+
+            # Aplica el estilo a la tabla
+            table.setStyle(style)
+
+            # Agrega la tabla a los elementos
+            elements.append(table)
+
+            # Construye el PDF (incluyendo la tabla)
+            doc.build(elements)
+
+            # Recupera el valor del objeto de tipo 'file'.
+            buffer.seek(0)
+            pdf = buffer.getvalue()
+            buffer.close()
+
+            response = FileResponse(pdf, as_attachment=False, filename='blank.pdf')
+            response['Content-Type'] = 'application/pdf'
+            return response
+
+
 
 
 @admin_required
