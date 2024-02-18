696ad403e45b84ae13de25c24aebd068facc462a
jfilippo06 <jfilippo04@gmail.com>
2024-02-18 04:09:39 +0100

    inventario

--- reporte/views.py
+++ reporte/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from login.decorators import admin_required, employee_denied
 from venta.models import Direccion_de_factura
+from inventario.models import Inventario
 from django.core.paginator import Paginator
 from reparacion.models import Reparacion
 from venta.models import Totales
@@ -180,19 +181,53 @@ def reporteCliente(request):
     elif request.method == 'POST':
         pass
 
-    return render(request, 'reporte/cliente.html', {'username': request.user.username})
+    return render(request, 'reporte/cliente.html', {'username': request.user.username})       
 
-
+   
 @admin_required
 @employee_denied
 def reporteInventario(request):
     if request.method == 'GET':
-        pass
+        if 'inventario_ads' in request.session:
+            inventario_ads = request.session['inventario_ads']
+            inventario = Inventario.objects.filter(id__in=inventario_ads)
+        else:
+            inventario = []
 
     elif request.method == 'POST':
-        pass
+        equipo = request.POST['equipo']
+        user_type = request.POST['user_type']
+        table_search = request.POST['table_search']
 
-    return render(request, 'reporte/inventario.html', {'username': request.user.username})
+        # Define a dictionary to map user_type to the corresponding field in the model
+        user_type_field_map = {
+            'codigo': 'codigo__iexact',
+            'articulo': 'articulo__iexact',
+            'marca': 'marca__iexact',
+            'modelo': 'modelo__iexact',
+            'no_serie': 'no_serie__iexact',
+            'cantidad': 'cantidad',
+            'costo': 'costo'
+        }
+
+        # Initialize the query parameters
+        query_params = {'categoria': equipo}
+
+        # If table_search is not empty, add the corresponding field to the query parameters
+        if table_search != '':
+            query_params[user_type_field_map[user_type]] = table_search
+
+        # Query the database
+        inventario = Inventario.objects.filter(
+            **query_params).exclude(is_active=False)
+        request.session['inventario_ads'] = list(
+            inventario.values_list('id', flat=True))
+
+    paginator = Paginator(inventario, 15)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'reporte/inventario.html', {'username': request.user.username, 'inventario': page_obj})
 
 
 @admin_required
