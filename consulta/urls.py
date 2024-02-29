017f2c8cafd50b387aec33cb4bb4339ebdac20f0
jfilippo06 <jfilippo04@gmail.com>
2024-02-29 01:58:44 +0100

    consulta

--- consulta/urls.py
+++ consulta/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('registrar_cliente/', views.registrar_cliente, name='registrar_cliente'),
+    path('consultar_cliente/', views.consultar, name='consultar_cliente'),
 ]
--- consulta/views.py
+++ consulta/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from login.decorators import admin_required, employee_denied
+from inventario.models import Inventario
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+@admin_required
+@employee_denied
+def consultar(request):
+    if request.method == 'GET':
+        if 'inventario_ids' in request.session:
+            inventario_ids = request.session['inventario_ids']
+            inventario = Inventario.objects.filter(id__in=inventario_ids)
+        else:
+            inventario = []
+
+    elif request.method == 'POST':
+        equipo = request.POST['equipo']
+        user_type = request.POST['user_type']
+        table_search = request.POST['table_search']
+
+        # Define a dictionary to map user_type to the corresponding field in the model
+        user_type_field_map = {
+            'codigo': 'codigo__iexact',
+            'articulo': 'articulo__iexact',
+            'marca': 'marca__iexact',
+            'modelo': 'modelo__iexact',
+            'no_serie': 'no_serie__iexact',
+            'cantidad': 'cantidad',
+            'costo': 'costo'
+        }
+
+        # Initialize the query parameters
+        query_params = {'categoria': equipo}
+
+        # If table_search is not empty, add the corresponding field to the query parameters
+        if table_search != '':
+            query_params[user_type_field_map[user_type]] = table_search
+
+        # Query the database
+        inventario = Inventario.objects.filter(
+            **query_params).exclude(is_active=False)
+        request.session['inventario_ids'] = list(
+            inventario.values_list('id', flat=True))
+
+    paginator = Paginator(inventario, 5)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'consulta/consultar.html', {'username': request.user.username, 'user_type': request.user.user_type, 'inventario': page_obj})
