57bf8935a3c95034bec19c15c0ebd976b8149765
jfilippo06 <jfilippo04@gmail.com>
2024-02-29 01:34:31 +0100

    cliente

--- cliente/views.py
+++ cliente/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from login.decorators import admin_required, employee_denied
+from django.contrib import messages
+from venta.models import Client
 
 # Create your views here.
+
+@admin_required
+@employee_denied
+def registrar_cliente(request):
+    if request.method == 'GET':
+        return render(request, 'cliente/registrar_cliente.html', {'username': request.user.username})
+
+    elif request.method == 'POST':
+        cedula = request.POST['cedula']
+        nombre = request.POST['nombre']
+        email = request.POST['email']
+
+        if Client.objects.filter(cedula=cedula).exists():
+            messages.error(request, 'Cédula ya exite.')
+            return redirect('registrar_cliente')
+        elif Client.objects.filter(email=email).exists():
+            messages.error(
+                request, 'Correo electronico le pertenece a otro cliente.')
+            return redirect('registrar_cliente')
+
+        nuevo_cliente = Client(cedula=cedula, username=nombre, email=email)
+        nuevo_cliente.save()
+
+        messages.success(request, 'Cliente registrado correctamente.')
+        return redirect('registrar_cliente')
--- venta/views.py
+++ venta/views.py
@@ -57,35 +57,7 @@ def client(request):
             return redirect('facturar_cliente')
         else:
             messages.error(request, 'Cliente no existe.')
-            return redirect('registrar_cliente')
-
-
-@admin_required
-@employee_denied
-def registrar_cliente(request):
-    if request.method == 'GET':
-        cedula = request.session.get('cedula')
-        return render(request, 'venta/registrar_cliente.html', {'username': request.user.username, 'cedula': cedula})
-
-    elif request.method == 'POST':
-        cedula = request.POST['cedula']
-        nombre = request.POST['nombre']
-        email = request.POST['email']
-
-        if Client.objects.filter(cedula=cedula).exists():
-            messages.error(request, 'Cédula ya exite.')
-            return redirect('registrar_cliente')
-        elif Client.objects.filter(email=email).exists():
-            messages.error(
-                request, 'Correo electronico le pertenece a otro cliente.')
-            return redirect('registrar_cliente')
-
-        nuevo_cliente = Client(cedula=cedula, username=nombre, email=email)
-        nuevo_cliente.save()
-
-        messages.success(request, 'Cliente registrado correctamente.')
-        request.session['last'] = get_last_n_factura()
-        return redirect('facturar_cliente')
+            return redirect('cliente')
 
 
 @admin_required
